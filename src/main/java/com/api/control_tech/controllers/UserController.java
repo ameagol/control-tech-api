160eb927a67e3af2351a40f748be43350d3dc001
ricgellman <ricaprado@yahoo.com>
2024-11-30 12:52:19 -0300

    Update UserController.java

--- src/main/java/com/api/control_tech/controllers/UserController.java
+++ src/main/java/com/api/control_tech/controllers/UserController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = {"http://localhost:4200", "https://control-tech-ui.vercel.app"})
+//@CrossOrigin(origins = {"http://localhost:4200", "https://control-tech-ui.vercel.app"})
 @RequestMapping("/api/")
 public class UserController {
 
