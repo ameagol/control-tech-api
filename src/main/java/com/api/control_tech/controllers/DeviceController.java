d3c44ff74a13fce1ffe5c26101c9e233df9c8b0f
ricgellman <ricaprado@yahoo.com>
2024-11-30 10:22:07 -0300

    Update DeviceController.java

--- src/main/java/com/api/control_tech/controllers/DeviceController.java
+++ src/main/java/com/api/control_tech/controllers/DeviceController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
  * ! Books resources with some simple validation
  */
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = {"http://localhost:4200", "https://control-tech-ui.vercel.app"})
 @RequestMapping("/api/devices")
 public class DeviceController {
 
