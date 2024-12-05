20eaa13a0910a5373fd74fcbc70795f8b1b5c07d
ricgellman <ricaprado@yahoo.com>
2024-12-05 13:35:38 -0300

    Update Device.java

--- src/main/java/com/api/control_tech/persistence/entities/Device.java
+++ src/main/java/com/api/control_tech/persistence/entities/Device.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 @Table(name = "devices")
 public class Device {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.UUID)
     private Long id;
 
     @CreationTimestamp
